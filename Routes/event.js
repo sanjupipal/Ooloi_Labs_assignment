90a0e21ee6346b89430d16b064bc6a3218849e8f
Sanju <sanjupipal@gmail.com>
2021-02-11 13:16:06 +0530

    validator

--- Routes/event.js
+++ Routes/event.js
@@ -3,10 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // validators
-const {
-  eventCreateValidator,
-  eventUpdateValidator,
-} = require("../validator/event");
+const { eventCreateValidator } = require("../validator/event");
 
 //middleware
 const { runValidation } = require("../validator/index");
@@ -17,7 +14,7 @@ const { create, read, update, remove } = require("../Controller/event");
 //routes
 router.post("/event", eventCreateValidator, runValidation, create);
 router.get("/event", read);
-router.put("/event/:id", update);
+router.put("/event/:id", eventCreateValidator, runValidation, update);
 router.delete("/event/:id", remove);
 
 module.exports = router;
--- validator/event.js
+++ validator/event.js
@@ -5,10 +5,3 @@ exports.eventCreateValidator = [
   check("link").not().isEmpty().withMessage("Registration link is required"),
   check("date").not().isEmpty().withMessage("Event date is required"),
 ];
-
-exports.eventUpdateValidator = [
-  check("name").not().isEmpty().withMessage("Name is required"),
-  check("content")
-    .isLength({ min: 20 })
-    .withMessage("Content is required of min 20 char"),
-];
